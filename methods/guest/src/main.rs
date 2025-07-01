075bf21900107ca7b82dfdea4cb7e6f928737e51
proof-partner <proofpartner6@gmail.com>
2025-06-30 21:13:01 -0400

    remove dependencies for running independently

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,35 +1,18 @@
 use risc0_zkvm::{guest::env, serde};
 
-// According to chatgpt, tiktok influencers are split into tiers. We'll define each one
-#[derive(Debug)]
-#[repr(u8)]
-enum InfluencerType {
-    /// Less than 1,000 followers.
-    None,
-    /// 1,000–10,000 followers.
-    Nano,
-    /// 10,001–50,000 followers.
-    Micro,
-    /// 50,001–500,000 followers.
-    MidTier,
-    /// 500,001–1,000,000 followers.
-    Macro,
-    /// 1,000,001+ followers.
-    Mega,
-}
-
-// https://www.tiktok.com/aweme/v2/data/insight/?locale=en&aid=1988&priority_region=US&region=US&tz_name=America%2FNew_York&app_name=tiktok_creator_center&app_language=en&device_platform=web_pc&channel=tiktok_web&device_id=7397803382726641198&os=mac&screen_width=1440&screen_height=900&browser_language=en-US&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0+(Macintosh%3B+Intel+Mac+OS+X+10_15_7)+AppleWebKit%2F537.36+(KHTML,+like+Gecko)+Chrome%2F137.0.0.0+Safari%2F537.36&tz_offset=-14400&type_requests=[%7B%22insigh_type%22:%22follower_num_history%22,%22days%22:16,%22end_days%22:1%7D,%7B%22insigh_type%22:%22follower_num%22,%22days%22:16,%22end_days%22:1%7D,%7B%22insigh_type%22:%22net_follower_history%22,%22days%22:16,%22end_days%22:1%7D]
+use primitives::AccountType;
 
 // The following code's execution trace will be proven with a zk stark
 fn main() {
     // Receive journal, for verifying the core proof of the tls notary signatures
     let journal: [u8; 32] = env::read();
     // Receive the entire TLS Notary representation
-    // TODO: Hash this with accelerated sha2 and compare to hash above(journal value)
+    // *SECURITY* TODO: Hash this with accelerated sha2 and assert it's equal to journal's value
     let tlsn_representation_bytes: Vec<u8> = env::read();
+
     // Program id for core tlsn verifier 
-    let tlsn_prover_id = [1363182423, 1400773265, 918797330, 1419448088, 1135916865, 604038609, 1578339176, 834135541] ;
-    
+    let tlsn_prover_id = [1997738335, 3678040518, 3768798354, 2157753599, 1334048300, 48065757, 3864380607, 3289501451];
+
     // Verify previous proof of our tls notary proof validity
     env::verify(tlsn_prover_id, &serde::to_vec(&journal).unwrap()).unwrap();
 
@@ -46,28 +29,22 @@ fn main() {
         &format!("Guest: got follower count {:?} from data", follower_val)
     );
 
-    // Identify the type of account based on amount of followers
-    let influencer_type = match follower_val {
-        0..=999 => InfluencerType::None,
-        1000..=10_000 => InfluencerType::Nano,
-        10_001..=50_000 => InfluencerType::Micro,
-        50_001..=500_000 => InfluencerType::MidTier,
-        500_001..=1_000_000 => InfluencerType::Macro,
-        _ => InfluencerType::Mega,
-    };
+    // Identify the account follower tier based on the number of followers
+    let account_type = AccountType::from_follower_count(follower_val);
 
-    // "Commit" the underlying value, making it public. Ensures that other parties can understand our return value, the influencer type
-    env::commit(&(influencer_type as u8));
+    // "Commit" the underlying value, making it public. Ensures that other parties can understand our return value, the account type. We emit this in an event field onchain.
+    env::commit(&(account_type as u8));
 }
 
 // After taking a look at the api response JSON, we identified the unique field we want to return. That field is a nested field `follower_num`.
 // We note that the same exists in the UTF-8 bytes of the response field of the session, which is added to the full bytes of the proof. 
-// That means we can search for it from the top-level(for example code simplicity).
+// That means we can search for it from the top-level(for example code simplicity), though in your implementation you may wish to search only the received bytes field.
 fn extract_follower_num_object(utf8_bytes: &[u8]) -> Option<&[u8]> {
     // Identify the field that we are interested in. `follower_num` is a json field object with a two keys underneath
-    let needle = b"\"follower_num\"";
-    let start_idx = utf8_bytes.windows(needle.len())
-        .position(|window| window == needle)?;
+    let desired_field = b"\"follower_num\"";
+
+    let start_idx = utf8_bytes.windows(desired_field.len())
+        .position(|window| window == desired_field)?;
 
     // Find the opening `{` after the key
     let mut brace_start = utf8_bytes[start_idx..].iter().position(|&b| b == b'{')?;
---
+++ primitives/src/lib.rs
@@ -0,0 +1,48 @@
+// According to chatgpt, tiktok influencers are split into tiers. We'll define each one
+#[derive(Debug)]
+#[repr(u8)]
+pub enum AccountType {
+    /// Less than 1,000 followers.
+    None,
+    /// 1,000–10,000 followers.
+    Nano,
+    /// 10,001–50,000 followers.
+    Micro,
+    /// 50,001–500,000 followers.
+    MidTier,
+    /// 500,001–1,000,000 followers.
+    Macro, 
+    /// 1,000,001+ followers.
+    Mega,
+}
+
+impl TryFrom<u8> for AccountType {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(AccountType::None),
+            1 => Ok(AccountType::Nano),
+            2 => Ok(AccountType::Micro),
+            3 => Ok(AccountType::MidTier),
+            4 => Ok(AccountType::Macro),
+            5 => Ok(AccountType::Mega),
+            _ => Err(()),
+        }
+    }
+}
+
+
+impl AccountType {
+    // Return an influencer type based on the amount of followers the account has
+    pub fn from_follower_count(follower_count: u32) -> Self {
+        match follower_count {
+            0..=999 => AccountType::None,
+            1000..=10_000 => AccountType::Nano,
+            10_001..=50_000 => AccountType::Micro,
+            50_001..=500_000 => AccountType::MidTier,
+            500_001..=1_000_000 => AccountType::Macro,
+            _ => AccountType::Mega,
+        }
+    }
+}
